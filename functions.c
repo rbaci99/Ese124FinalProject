e61a6bfccb0df5c2f7dd7b2bc0cbdf9c22b36b50
Robert Bacigalupo <62004531+rbaci99@users.noreply.github.com>
2021-05-13 01:06:21 -0400

    Update functions.c (#5)

--- functions.c
+++ functions.c
@@ -1,8 +1,51 @@
+//Functions for Michael
 #include <iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-//Functions for Michael
+
+#include"stack.h";
+//marks current position as 'p' for pheremone
+//make sure to follow all moves by mark
+void MARK(int row,int col,char *maze){
+	maze[row][col]='p';
+	
+}//MARK
+
+//moves ant foward by 1 in x direction
+void MOVE_F(){
+ //add check in main file to see if maze is *   ??should we put check in here instead
+ //add in main so that way we can print to file if invalid spot
+	int x = stackX[top];
+	int y = stackY[top];
+	push(x+1,y);
+}//MOVE_F
+
+//moves ant backwards by 1 in x direction
+void MOVE_B(){
+	int x = stackX[top];
+	int y = stackY[top];
+	push(x-1,y);
+}//MOVE_B
+
+//moves ant left by 1 in y direction
+void MOVE_L(){
+	int x = stackX[top];
+	int y = stackY[top];
+	push(x,y-1);
+}//MOVE_L
+
+//moves ant right by 1 in y direction
+void MOVE_R(){
+	int x = stackX[top];
+	int y = stackY[top];
+	push(x,y+1);
+}//MOVE_R
+//checks for itch to left and returns number of free spaces
+int CWL(char *maze){
+	
+}
+
 
 
 
